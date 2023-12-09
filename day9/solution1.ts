73a01f74128c5a68b2fa8aeca3c59da5b7a7f662
Flinn Burgess <flinnburgess@gmail.com>
2023-12-09 07:39:08 +0000

    solution to day 9

---
+++ day9/solution1.ts
@@ -0,0 +1,39 @@
+import * as fs from "node:fs";
+import * as readline from "node:readline";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { convertLineToNumberArray } from "../utils";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const inputStream = fs.createReadStream(`${__dirname}/input.txt`);
+const rl = readline.createInterface({
+  input: inputStream,
+  crlfDelay: Infinity,
+});
+
+let answer = 0;
+
+const mapToDifferences = (numbers: number[]) => {
+  const differences: number[] = [];
+  for (let i = 1; i < numbers.length; i++) {
+    differences.push(numbers[i] - numbers[i - 1]);
+  }
+  return differences;
+};
+
+const findNextInSequence = (numbers: number[]) => {
+  if (numbers.find((num) => num !== 0) === undefined) {
+    return 0;
+  }
+
+  return numbers.at(-1) + findNextInSequence(mapToDifferences(numbers));
+};
+
+for await (const line of rl) {
+  const numbers = convertLineToNumberArray(line);
+  answer += findNextInSequence(numbers);
+}
+
+console.log(answer);
